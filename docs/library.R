2f8fb4f969a3c9cb29ab19c2a2630f5e733629b5
Garrick Aden-Buie <garrick.aden-buie@moffitt.org>
2020-08-28 15:28:59 -0400

    Add meta tags and social card

--- docs/library.R
+++ docs/library.R
@@ -2,6 +2,10 @@ if (!requireNamespace("xaringanExtra", quietly = TRUE)) {
   stop("`xaringanExtra` is required: devtools::install_github('gadenbuie/xaringanExtra')")
 }
 
+if (!requireNamespace("metathis", quietly = TRUE)) {
+  stop("`metathis` is required: devtools::install_github('gadenbuie/metathis')")
+}
+
 if (!requireNamespace("moffittdocs", quietly = TRUE)) {
   stop("`moffittdocs` is required: devtools::install_github('Moffitt-Health-Informatics/moffittdocs')")
 }
